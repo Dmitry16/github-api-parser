a974c39a9217dab4fca04dcaabbd9b4ec22d5a28
Dmitry16 <europartner@inbox.ru>
2018-12-02 07:59:58 +0200

    dataHandler_bag_fix

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -4,41 +4,40 @@ const { parser } = require('stream-json')
 const { streamArray } = require('stream-json/streamers/StreamArray')
 const { streamValues } = require('stream-json/streamers/StreamValues')
 const errorHandler = require('../errorHandlers')
+let resourceCounter = 0
+let chunksLength = 0
+let statsObj = {}
 
 async function fetchData(
-    period, 
-    conStrBase, 
-    conStrParam, 
-    conParams
+  period, 
+  conStrBase, 
+  conStrParam, 
+  conParams
 ){
-    let chunksLength = 0
-    let statsObj = {}
-    let resourceCounter = 0
+  //instantiating stream for filtering and writing data
+  const createProcessingStream = require('../streamModifier')
 
-    //instantiating stream for filtering and writing data
-    const createProcessingStream = require('../streamModifier')
-  
-    const input =
-      conStrParam !== '/rate_limit' ? conStrBase + conStrParam : conStrParam
-  
-    await axios
-      .get(input, conParams)
-      .then(response => {
-        resourceCounter++
-        let contLength = response.headers['content-length']
-        let processingStream = createProcessingStream(
-          contLength,
-          resourceCounter,
-          chunksLength,
-          statsObj,
-          period,
-        )
-        response.data
-          .pipe(parser())
-          .pipe(resourceCounter !== 5 ? streamArray() : streamValues())
-          .pipe(processingStream)
-      })
-      .catch(errorHandler)
-  }
+  const input =
+    conStrParam !== '/rate_limit' ? conStrBase + conStrParam : conStrParam
 
-  module.exports = fetchData
+  await axios
+    .get(input, conParams)
+    .then(response => {
+      resourceCounter++
+      let contLength = response.headers['content-length']
+      let processingStream = createProcessingStream(
+        contLength,
+        resourceCounter,
+        chunksLength,
+        statsObj,
+        period,
+      )
+      response.data
+        .pipe(parser())
+        .pipe(resourceCounter !== 5 ? streamArray() : streamValues())
+        .pipe(processingStream)
+    })
+    .catch(errorHandler)
+}
+
+module.exports = fetchData
--- src/helpers/dataFilter.js
+++ src/helpers/dataFilter.js
@@ -1,11 +1,13 @@
 function dataHandler(obj, resourceCounter, statsObj) {
   if (resourceCounter < 4) {
+    // console.log('zzzzz', resourceCounter)
     if (!statsObj[obj.value.user.login]) {
       statsObj[obj.value.user.login] = [1, 0]
     } else if (statsObj[obj.value.user.login]) {
       statsObj[obj.value.user.login][0]++
     }
   } else if (resourceCounter === 4 && statsObj[obj.value.author.login]) {
+    // console.log('xxxxx', resourceCounter)
     statsObj[obj.value.author.login][1] = obj.value.total
   } else if (resourceCounter === 5) {
     statsObj.remaining = obj.value.rate.remaining
